0b12dea10dd7fff086b7104c4b89e849fca97856
AlvaroMbeiaDaniel1234 <alvarombeiadanielmiguel@gmail.com>
2025-09-16 01:40:33 +0100

    feat: adicionei a api para consultar os documentos do tipi bi

--- app/controllers/verificacoa_bis_controller.ts
+++ app/controllers/verificacoa_bis_controller.ts
@@ -1,25 +1,22 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import axios from 'axios'
 
-// app/Controllers/Http/VerificacoaBisController.ts
-
-import axios from 'axios';
-
-const EXTERNAL_API_URL = 'https://consulta.edgarsingui.ao/consultar';
+const EXTERNAL_API_URL = 'https://consulta.edgarsingui.ao/consultar'
 
 export default class VerificacoaBisController {
-  public async index({ params, response }: HttpContext) {
-    const numero = params.numero;
+  public async index({ request, response }: HttpContext) {
+    const numero = request.input('numero')
 
     if (!numero) {
-      return response.status(400).json({ error: true, message: 'Número de consulta é obrigatório' });
+      return response.status(400).json({ error: true, message: 'Número de consulta é obrigatório' })
     }
 
     try {
-      const apiResponse = await axios.get(`${EXTERNAL_API_URL}/${numero}`);
-      return response.json(apiResponse.data);
+      const apiResponse = await axios.get(`${EXTERNAL_API_URL}/${numero}`)
+      return response.json(apiResponse.data)
     } catch (error) {
-      console.error('Erro ao consultar API externa:', error);
-      return response.status(500).json({ error: true, message: 'Erro interno ao processar a consulta' });
+      console.error('Erro ao consultar API externa:', error)
+      return response.status(500).json({ error: true, message: 'Erro interno ao processar a consulta' })
     }
   }
 }
